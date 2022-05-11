2523e14b8a92d37435e8b930c2ce1ff6d8808274
Quy Lang <quylang88@gmail.com>
2022-05-11 13:45:14 +0900

    logo編集

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 window.onscroll = function () {
     document.getElementById("navFixed").style.opacity = "1";
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("navFixed").style.transform = "translate(0,-177%)";
+        document.getElementById("navFixed").style.transform = "translate(0,-190%)";
     } else {
-        document.getElementById("navFixed").style.transform = "translate(0,-300%)";
+        document.getElementById("navFixed").style.transform = "translate(0,-350%)";
     }
 };

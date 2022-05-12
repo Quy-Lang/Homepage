eedef281f0e302576a7ee3f6c05238f6777f37ac
Quy Lang <quylang88@gmail.com>
2022-05-12 13:53:17 +0900

    animate 追加

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+new WOW().init();
+
 window.onscroll = function () {
     document.getElementById("navFixed").style.opacity = "1";
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
@@ -5,4 +7,17 @@ window.onscroll = function () {
     } else {
         document.getElementById("navFixed").style.transform = "translate(0,-350%)";
     }
+};
+
+var mainNav = document.getElementById("mainNav");
+var listNav = mainNav.getElementsByTagName("li");
+
+var mainNavFixed = document.getElementById("mainNavFixed");
+var listNavFixed = mainNavFixed.getElementsByTagName("li");
+for (var i = 0; i < listNavFixed.length; i++) {
+    listNavFixed[i].addEventListener("click", function () {
+        var currentFixed = document.querySelector("#mainNavFixed .active");
+        currentFixed.className = currentFixed.className.replace("active", "");
+        this.className += " active";
+    });
 };
